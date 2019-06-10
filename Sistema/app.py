db33141c66491c3dcbd6bf0805b4359af7bd6843
Pierre Victor <pierre.sousa@ijunior.com.br>
2019-06-10 03:16:04 -0300

    Herança de template com jinja

--- Sistema/app.py
+++ Sistema/app.py
@@ -1,1 +1,14 @@
-print("Hello word")
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/sobre")
+def sobre():
+    return render_template('sobre.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
