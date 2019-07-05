b7e290b4ac6b8f563eae8681824ba212e5139b85
Pierre Victor <pierre.sousa@ijunior.com.br>
2019-07-05 11:54:55 -0300

    qualquercoisa

--- administer/administer/__init__.py
+++ administer/administer/__init__.py
@@ -37,7 +37,7 @@ def login_required(role=["ANY"]):
 
             if not current_user.is_authenticated:
                return current_app.login_manager.unauthorized()
-            urole = current_app.login_manager.reload_user().get_urole()
+            urole = current_user.get_urole()
             if ( (urole not in role) and (role != ["ANY"])):
                 return current_app.login_manager.unauthorized()      
             return fn(*args, **kwargs)
