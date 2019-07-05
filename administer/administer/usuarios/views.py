d99f688e63624ced90f3ac1b99a3fc3f34da30a5
Pierre Victor <pierre.sousa@ijunior.com.br>
2019-07-05 19:11:07 -0300

    Perfil e base

--- administer/administer/usuarios/views.py
+++ administer/administer/usuarios/views.py
@@ -90,7 +90,21 @@ def perfil():
 	
 	add_funcionario = funcionario_form()
 	editar_user = EditarUserForm()
-	
+
+	if editar_user.validate_on_submit(): 
+		bcript = Bcrypt()		
+		current_user.nome = editar_user.nome.data
+		current_user.username = editar_user.username.data
+		current_user.email = editar_user.email.data
+		current_user.data_nasc = editar_user.data_nascimento.data
+
+		current_user.hhash = bcript.generate_password_hash(editar_user.senha.data)
+
+		current_user.avatar = adicionar_avatar(editar_user.foto.data, editar_user.username.data) 
+		db.session.commit()
+
+		flash("Dados atualizados!","success")
+
 	return render_template("perfil.html", add_funcionario=add_funcionario, editar_user=editar_user)
 
 @login_required()
