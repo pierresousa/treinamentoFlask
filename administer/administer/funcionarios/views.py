aa13a24cdfbd51b54664d5bf48cb73b7e2acee2a
Pierre Victor <pierre.sousa@ijunior.com.br>
2019-07-06 00:38:43 -0300

    Correção de bugs

--- administer/administer/funcionarios/views.py
+++ administer/administer/funcionarios/views.py
@@ -112,10 +112,12 @@ def exibe_all():
 	add_funcionario = funcionario_form()
 
 	titulo = "Todos funcionários"
+	setor = [("0", "Equipe administrativo"), ("1", "Desenvolvedor"), ("2", "Equipe projetos"), ("3", "Equipe RH"), ("4", "Equipe marketing"), ("5", "Equipe presidencia"), ("6", "Equipe Negocios")]
+	setor = dict(setor)
 
 	page = request.args.get('page', 1, type=int)
 	funcionarios = Funcionario.query.paginate(page=page, per_page=12)
-	return render_template("todos_funcionarios.html", titulo=titulo, funcionarios=funcionarios, add_funcionario=add_funcionario)
+	return render_template("todos_funcionarios.html", setor=setor, titulo=titulo, funcionarios=funcionarios, add_funcionario=add_funcionario)
 
 @login_required()
 @funcionarios.route("/meus_funcionarios")
@@ -124,11 +126,13 @@ def meus_funcionarios():
 	add_funcionario = funcionario_form()
 
 	titulo = "Meus funcionários"
+	setor = [("0", "Equipe administrativo"), ("1", "Desenvolvedor"), ("2", "Equipe projetos"), ("3", "Equipe RH"), ("4", "Equipe marketing"), ("5", "Equipe presidencia"), ("6", "Equipe Negocios")]
+	setor = dict(setor)
 
 	page = request.args.get('page', 1, type=int)
 	funcionarios = Funcionario.query.filter_by(admin_id=current_user.id).paginate(page=page, per_page=12)
 	
-	return render_template("todos_funcionarios.html", titulo=titulo, funcionarios=funcionarios, add_funcionario=add_funcionario)
+	return render_template("todos_funcionarios.html", setor=setor, titulo=titulo, funcionarios=funcionarios, add_funcionario=add_funcionario)
 
 @login_required()
 @funcionarios.route("/exibe/<int:id>")
